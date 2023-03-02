52363b1a374d39d8010517d8ad8b06ea250cb299
Rodrigo Alonso Solaguren-Beascoa <44756216+ralonsobeas@users.noreply.github.com>
2023-03-02 16:03:16 +0100

    Añadido paquete de nieve

---
+++ Assets/Scripts/GlobalSnow.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class GlobalSnow : MonoBehaviour
+{
+    [Range(0.1f, 1f)]
+    public float globalSnowAmount = 0.5f;
+
+    [Range(0.1f, 1f)]
+    public float snowBlendStrength;
+
+
+    public Color snowColorUpper;
+    public Color snowColorBottom;
+
+    public void Start()
+    {
+        SetValues();
+    }
+
+    public void OnValidate()
+    {
+        SetValues();
+    }
+
+    private void FixedUpdate()
+    {
+        SetValues();
+    }
+
+    private void SetValues()
+    {
+        Shader.SetGlobalFloat("_GlobalSnowAmount", globalSnowAmount);
+        Shader.SetGlobalFloat("_TerrainSnowBlendStrength", snowBlendStrength);
+
+        Shader.SetGlobalColor("_snowColorUpper", snowColorUpper);
+        Shader.SetGlobalColor("_snowColorBottom", snowColorBottom);
+    }
+}
