ee189fc2a6ee640160c1fa968691717a29ccfa77
Adrián Naranjo <adriannaranjosanchez@gmail.com>
2023-03-22 23:29:59 +0100

    prefab created

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,7 +16,7 @@ public class GameManager : Singleton
     {
         OnGameStart();
     }
-
+   
     public void DecreaseLife()
     {
         OnDecreaseLife();
--- Assets/Scripts/Managers/HealthManager.cs
+++ Assets/Scripts/Managers/HealthManager.cs
@@ -4,21 +4,30 @@ using UnityEngine;
 public class HealthManager : MonoBehaviour
 {
     //TODO change in future to glasses property and allows inventary
-    public int numLifes = 3;
+    public GameObject[] glasses_image_states; 
+    public int numDamage = 0;
     // secs to clean glasses
     public int timeToCleanGlasses = 300;
     void Start()
     {
         ((GameManager)GameManager.Instance).OnGameStart += EquipGlasses;
+        ((GameManager)GameManager.Instance).StartGame();
     }
 
     public void EquipGlasses()
     {
+        HideAllGlasses();
+        UnHidePositionGlasses(0);
         // Show picture glasses       
     }
 
+    [ContextMenu("DecreaseLife")]
     public void BreakGlasses()
     {
+        if (numDamage >= 3) return;
+        numDamage++;
+        HideAllGlasses();
+        UnHidePositionGlasses(numDamage);
         // change picture broken and frozen glasses
     }
 
@@ -35,5 +44,18 @@ public class HealthManager : MonoBehaviour
     IEnumerator TimeToFogUpGlasses(int secs) {
         yield return new WaitForSeconds(secs);
         // show foggy glasses
+    }
+    
+    private void HideAllGlasses()
+    {
+        for(int i = 0; i < glasses_image_states.Length; i++)
+        {
+           glasses_image_states[i].SetActive(false); 
+        }
+    } 
+    
+    private void UnHidePositionGlasses(int position)
+    {
+        glasses_image_states[position].SetActive(true);
     }
 }
