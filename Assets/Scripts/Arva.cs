d37dc82e5c940def7f7ff5f6883fd5dd9e0cdab6
Jaime De Carvajal Morillo <madmovesl@gmail.com>
2023-03-23 17:21:01 +0100

    arva

--- Assets/Scripts/Arva.cs
+++ Assets/Scripts/Arva.cs
@@ -8,7 +8,6 @@ using UnityEngine.UI;
 
 public class Arva : MonoBehaviour
 {
-    [SerializeField]
     private Transform playerPos;
 
     [SerializeField]
@@ -30,6 +29,7 @@ public class Arva : MonoBehaviour
     {
         arvaArrow.sprite = arrows[0];
         arvaNumber.text = "99999";
+        playerPos = this.transform;
     }
 
     private void Update()
@@ -40,7 +40,7 @@ public class Arva : MonoBehaviour
     private void updateArva()
     {
         arvaNumber.text = ((int)Mathf.Min(Mathf.Round(Vector3.Magnitude(victimPos.position - playerPos.position)),99999)).ToString();
-        actualAngle = Vector3.Angle(playerPos.forward, new Vector3(victimPos.position.x, playerPos.position.y, victimPos.position.z) - playerPos.position);
+        actualAngle = Vector3.SignedAngle(playerPos.forward, new Vector3(victimPos.position.x, playerPos.position.y, victimPos.position.z) - playerPos.position, playerPos.forward);
 
         //if (actualAngle < 0) actualAngle = 360f + actualAngle;
 
