6b735dcbd29e497bedd73b8d1b5e5f0ad57d525c
oscarbaltasar <oscarbaltasar@hotmail.com>
2023-03-30 16:26:16 +0200

    añadido calculo con textura para altura del jugador

--- Assets/Scripts/TerrainGravity.cs
+++ Assets/Scripts/TerrainGravity.cs
@@ -6,11 +6,16 @@ public class TerrainGravity : MonoBehaviour
 {
     [SerializeField] private float timeMultiplier = 100f;
     private TimedTrailRenderer[] trails;
+    private Texture2D snowHeightMap;
+
+    public int imageSize = 1024;
+    public int terrainSize = 128;
 
     // Start is called before the first frame update
     void Start()
     {
         trails = FindObjectsOfType<TimedTrailRenderer>();
+        snowHeightMap = FindObjectOfType<Terrain>().terrainData.alphamapTextures[0];
     }
 
     // Update is called once per frame
@@ -32,7 +37,16 @@ public class TerrainGravity : MonoBehaviour
                 minHeight = y;
         }
 
-        print((y));
-        return hasTrail ? Terrain.activeTerrain.SampleHeight(transform.position) + GlobalSnow.GlobalSnowOffset * (y) : Terrain.activeTerrain.SampleHeight(transform.position) + GlobalSnow.GlobalSnowOffset;
+
+        float redAddition = 0;
+        for (int i = -1; i < 2; i++)
+            for (int j = -1; j < 2; j++)
+            {
+                Color positionColor = snowHeightMap.GetPixel((int)(transform.position.x * imageSize / terrainSize)+i, (int)(transform.position.z * imageSize / terrainSize)+j);
+                redAddition += positionColor.r;
+            }
+        redAddition /= 9;
+
+        return hasTrail ? Terrain.activeTerrain.SampleHeight(transform.position) + GlobalSnow.GlobalSnowOffset * (y) * redAddition : Terrain.activeTerrain.SampleHeight(transform.position) + GlobalSnow.GlobalSnowOffset * redAddition;
     }
 }
