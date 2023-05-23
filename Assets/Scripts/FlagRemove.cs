1a3184f004e31e6d494e2380878985e11901720b
carvas6 <madmovesl@gmail.com>
2023-05-23 20:34:11 +0200

    flag infinite

--- Assets/Scripts/FlagRemove.cs
+++ Assets/Scripts/FlagRemove.cs
@@ -7,11 +7,40 @@ public class FlagRemove : MonoBehaviour
     [SerializeField]
     private GameObject flagCloth;
 
+    [SerializeField]
+    private GameObject completeFlag;
+
+    [SerializeField]
+    private Transform cinturonParent;
+
+    [SerializeField]
+    private Vector3 initialBeltPosition;
+
     private bool desplegada = false;
+
+    private bool isGrounded = false;
 
     public void plegarDesplegar()
     {
         desplegada = !desplegada;
         flagCloth.SetActive(desplegada);
+    }
+
+    public void grounded()
+    {
+        if(isGrounded) GameObject.Instantiate(completeFlag, this.transform.position, Quaternion.identity);
+
+        this.transform.SetParent(cinturonParent);
+        this.transform.position = initialBeltPosition;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other is TerrainCollider) isGrounded = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other is TerrainCollider) isGrounded = false;
     }
 }
