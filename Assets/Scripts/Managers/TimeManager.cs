e5ab24a8392a1f95888dbd83b9d56ab518154a93
Adrián Naranjo <adriannaranjosanchez@gmail.com>
2023-05-23 20:33:57 +0200

    fix error in time manager with flag

--- Assets/Scripts/Managers/TimeManager.cs
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -11,9 +11,21 @@ public class TimeManager : MonoBehaviour
     [SerializeField] private int secondAlertToDie = 40;
     [SerializeField] private int missionSecs = 200;
     private float time;
+    private bool alertIsSend;
+
+    private void Awake()
+    {
+        ((GameManager)GameManager.Instance).OnVictimIsRescued += VictimIsRescued;
+    }
+
     void Start()
     {
         MissionStart();
+    }
+
+    public void VictimIsRescued()
+    {
+        ((GameManager)GameManager.Instance).WinMission((int)time);
     }
 
     public void MissionStart()
@@ -40,10 +52,11 @@ public class TimeManager : MonoBehaviour
     private void Update()
     {
         time += Time.deltaTime;
-        if (time > missionSecs)
+        if (time > missionSecs && !alertIsSend)
         {
             ((GameManager)GameManager.Instance).FireAlert(Alert.VictimProbablyIsDied);
             ((GameManager)GameManager.Instance).VictimIsDied();
+            alertIsSend = true;
         }
     }
 }
